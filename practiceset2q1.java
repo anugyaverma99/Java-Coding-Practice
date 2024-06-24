d70487232d5a8ab8b1593ae19c03646fe76df09e
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-06-24 21:37:39 +0530

    Add files via upload

---
+++ practiceset2q1.java
@@ -0,0 +1,8 @@
+// example of precedence and associativity of operator
+
+public class practiceset2q1 {
+    public static void main(String args[]){
+        System.out.println(7/4*9/2);
+
+    }
+}

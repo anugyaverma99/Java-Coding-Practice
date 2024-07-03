687d38dcd3a470114b1176b21465d6891e4e2ba5
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-03 22:49:39 +0530

    Add files via upload

---
+++ strings1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class strings1 {
+    public static void main(String args[]){
+        // Scanner s=new Scanner(System.in);
+        // String str=s.next();
+        // System.out.printf("The name of the person is: %s",str);
+        float a=3.4f;
+        System.out.printf("the value of a is: %8.3f",a);
+    }
+}

19676d318ff3c2c4b5a34a35d48b9aea286ec2b3
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-04 10:46:55 +0530

    Add files via upload

---
+++ strings2.java
@@ -0,0 +1,35 @@
+//STRING METHODS IN JAVA
+
+public class strings2 {
+    public static void main(String args[]){
+        String name="Harry";
+        // System.out.println(name.length());
+        // System.out.println(name.toLowerCase());
+        // System.out.println(name.toUpperCase());
+        // String s= "    adef           ";
+        // System.out.println(s);
+        // System.out.println(s.trim());
+        // System.out.println(name.substring(3));
+        // System.out.println(name.substring(0,4));
+        // System.out.println(name.replace('r','p'));
+        // System.out.println(name.startsWith("Ha"));
+        // System.out.println(name.endsWith("ri"));
+        // System.out.println(name.charAt(2));
+        // System.out.println(name.indexOf("ry"));
+        // String s1="Harryrrry";
+        // System.out.println(s1.indexOf("rry"));
+        // System.out.println(s1.indexOf("rry",4));
+        // System.out.println(s1.indexOf("rrt"));
+        // System.out.println(name.lastIndexOf('r'));
+        // System.out.println(name.lastIndexOf("rr"));
+        // System.out.println(name.lastIndexOf("r",2));
+        // System.out.println(name.equals("Harry"));
+        // System.out.println(name.equalsIgnoreCase("haRRy"));
+
+// ESCAPE SEQUENCE CHARACTERS 
+
+         System.out.println("I am escape sequence double quote\",backslash\\ ,tab character \t ,new line\n");
+
+   
+    }
+}

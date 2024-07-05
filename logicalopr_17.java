c95a584c7e1931325992d6458d625d24c4b96d9f
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-05 14:59:52 +0530

    Add files via upload

---
+++ logicalopr_17.java
@@ -0,0 +1,36 @@
+public class logicalopr_17 {
+    public static void main(String args[]){
+        boolean a=true;
+        boolean b=false;
+        boolean c=false;
+        // System.out.println("FOR LOGIAL AND...");
+        // if(a && b && c){
+        //     System.out.println("y");
+        // }
+        // else{
+        //     System.out.println("N");
+        // }
+        // System.out.println("FOR LOGIAL OR...");
+        
+        // if(a || b || c){
+        //     System.out.println("y");
+        // }
+        // else{
+        //     System.out.println("N");
+        // }
+
+        // System.out.println("FOR LOGIAL NOT...");
+        
+        // if(b==!a){
+        //     System.out.println("y");
+        // }
+        // else{
+        //     System.out.println("N");
+        // }
+
+
+ 
+
+
+    }
+}

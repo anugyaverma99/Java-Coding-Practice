307664770ee1ebb7a0805d610cd71b6fd139daad
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-05 14:59:30 +0530

    Add files via upload

---
+++ conditionals_16.java
@@ -0,0 +1,22 @@
+public class conditionals_16 {
+    public static void main(String args[]){
+        //int age=19;
+        // if(age>18){
+        //     System.out.println("Yes,you can drive");
+            
+        // }
+        // else{
+        //     System.out.println("No, you cannot drive yet");
+        // }
+        int age=56;
+        if(age>56){
+            System.out.println("you are experienced");
+        }
+        else if(age>24){
+            System.out.println("you are semi-experienced");
+        }
+        else
+        System.out.println("you are not experienced");
+    }
+}
+

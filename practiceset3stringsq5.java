ceb98890c7de6f4ba7006c25b2ad766784ae8fe4
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-05 15:01:14 +0530

    Add files via upload

---
+++ practiceset3stringsq5.java
@@ -0,0 +1,9 @@
+// write the java program to format the given string in the form of letter with the help of escape sequence characters.
+
+public class practiceset3stringsq5 {
+    public static void main(String args[]){
+        
+    String str="Dear harry,\n\t This java course is nice.\n Thanks!";
+    System.out.println(str);
+    }
+}

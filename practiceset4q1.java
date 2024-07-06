6b71ba9bff91018ed268ac9df4ad7f3a452c0d5e
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-06 15:24:39 +0530

    Add files via upload

---
+++ practiceset4q1.java
@@ -0,0 +1,13 @@
+// what will be the output of the following program.
+
+public class practiceset4q1 {
+    public static void main(String args[]){
+        int a=10;
+        if(a==11){
+            System.out.println("Iam 11");
+        }
+        else{
+            System.out.println("I am not 11");
+        }
+    }
+}

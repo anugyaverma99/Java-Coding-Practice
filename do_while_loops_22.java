4f5607e39b710bf35f7c0fbcc5e8aa784aac3b15
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-08 19:52:14 +0530

    Add files via upload

---
+++ do_while_loops_22.java
@@ -0,0 +1,31 @@
+public class do_while_loops_22 {
+    public static void main(String[] args) {
+        // int i=0;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }
+        // while(i<9);
+
+        // int i=10;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+            
+        // }
+        
+        // while(i<9);
+
+        //write a java program to print first ten natural numbers using do while loop.
+        int c=1;
+        do{
+            System.out.println(c);
+            c++;
+        }while(c<=10);
+
+
+
+
+
+    }
+}

a72ca60952df2a53992ce10c0fe83aaa26efb5aa
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-08 19:52:43 +0530

    Add files via upload

---
+++ loops_21.java
@@ -0,0 +1,20 @@
+public class loops_21 {
+    public static void main(String[] args) {
+        
+        // WHILE LOOP
+        //int i=0;
+        // while(i<3){
+            //     System.out.println(i);
+            //     i++;
+            // }
+    
+     // write a java program to print natural numbers from 100 to 200.
+
+        System.out.println("Natural numbers from 100 to 200");
+        int i=100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}

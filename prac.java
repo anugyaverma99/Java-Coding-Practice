b907c9104fbafb9157a057480334bb0f4a54890b
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-09 12:39:49 +0530

    Add files via upload

---
+++ prac.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class prac {
+    public static void main(String args[]){
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n1=s.nextInt();
+        System.out.println("Square of the number is: ");
+        System.out.println(n1*n1);
+
+
+    }
+}

aa956c5cf8377d35f370cffb978cdf04e7fc0751
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-09 12:38:02 +0530

    Add files via upload

---
+++ break_and_continue_24.java
@@ -0,0 +1,32 @@
+public class break_and_continue_24 {
+    public static void main(String[] args) {
+        // for(int i=0;i<5;i++){
+        //     System.out.println(i);
+        //     System.out.println("java is great");
+        //     if(i==2){
+        //         System.out.println("ending the loop");
+        //         break;
+        //     }
+        // }
+    //     int i=0;
+    //     while(i<5){
+    //         System.out.println(i);
+    //             System.out.println("java is great");
+    //             if(i==2){
+    //                 System.out.println("ending the loop");
+    //                 break;
+    //             }
+    //             i++;
+    // }
+    // CONTINUE STATEMENT
+    for(int i=0;i<5;i++){
+        if(i==2){
+            System.out.println("ending the loop");
+            continue;
+        }
+        System.out.println(i);
+        System.out.println("java is great");
+        }
+        }
+    }
+

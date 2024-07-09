9757bfec356de55e2398abe2028dd4143f7e1b62
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-09 12:37:00 +0530

    Update for_loop_23.java

--- for_loop_23.java
+++ for_loop_23.java
@@ -6,8 +6,14 @@ public class for_loop_23 {
         // }
 
         // write a java program to print first n odd numbers using for loop.
-        // for(int i=1;i<=20;i=i+2){
+        // int n=20;
+        // int c=1;
+        // for(int i=1;c<=n;i=i+1){
+        //     if(i%2 !=0){
         //     System.out.println(i);
+        //     c+=1;
+
+        //     }
         // }
 
         //Decrementing for-loop
@@ -16,13 +22,13 @@ public class for_loop_23 {
         //     System.out.println(i);
         // }
          
-        //wap to print first n natural numbsers in java.
-        Scanner sc=new Scanner(System.in);
-        System.out.print("enter a number");
-        int n=sc.nextInt();
-        for(int i=n;i>=0;i--){
-            System.out.println(i);
-        }
+        //wap to print first n natural numbsers in reverse in java.
+        // Scanner sc=new Scanner(System.in);
+        // System.out.print("enter a number");
+        // int n=sc.nextInt();
+        // for(int i=n;i>=0;i--){
+        //     System.out.println(i);
+        // }
 
 
 

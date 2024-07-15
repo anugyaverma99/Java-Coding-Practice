8c79eb5867a38cda3122ef8ac4c0a1c541f7f244
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-07-15 19:55:43 +0530

    Add files via upload

---
+++ arrays_26.java
@@ -0,0 +1,12 @@
+public class arrays_26 {
+    public static void main(String[] args) {
+        int[] marks=new int[5];
+        marks[0]=100;
+        marks[1]=90;
+        marks[2]=80;
+        marks[3]=70;
+        System.out.println(marks[3]);
+
+
+    }
+}

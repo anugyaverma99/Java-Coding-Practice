8dcfab16deb8468d212a3fba77055b94d01f993e
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-11-02 00:31:46 +0530

    Add files via upload

---
+++ practiceset7q4.java
@@ -0,0 +1,11 @@
+// write the java program to print the reverse star pattern
+public class practiceset7q4 {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

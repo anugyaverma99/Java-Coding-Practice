ff0fb62df3bfb8bb8e781df6021cc3a61c0b8f01
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2024-11-03 00:07:54 +0530

    Add files via upload

---
+++ practiceset7q9.java
@@ -0,0 +1,11 @@
+// convert celsius to fahrenheit.
+
+public class practiceset7q9 {
+
+    static int cel_to_fah(int c){
+        return ((c*9/5)+32);
+    }
+    public static void main(String[] args) {
+        System.out.println(cel_to_fah(32));
+    }
+}
